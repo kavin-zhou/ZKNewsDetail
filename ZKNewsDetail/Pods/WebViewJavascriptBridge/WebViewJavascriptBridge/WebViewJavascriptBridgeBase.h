22ff05b6602542bfeec7c9b2c29a4880bbe0094f
ZK <dev.zhoukang@gmail.com>
2017-04-17 14:22:59 +0800

    pod 'WebViewJavascriptBridge', '~> 5.0'

--- ZKNewsDetail/Pods/WebViewJavascriptBridge/WebViewJavascriptBridge/WebViewJavascriptBridgeBase.h
+++ ZKNewsDetail/Pods/WebViewJavascriptBridge/WebViewJavascriptBridge/WebViewJavascriptBridgeBase.h
@@ -7,9 +7,9 @@
 
 #import <Foundation/Foundation.h>
 
-#define kCustomProtocolScheme @"http"
-#define kQueueHasMessage      @"__wvjb_queue_message__"
-#define kBridgeLoaded         @"__bridge_loaded__"
+#define kCustomProtocolScheme @"wvjbscheme"
+#define kQueueHasMessage      @"__WVJB_QUEUE_MESSAGE__"
+#define kBridgeLoaded         @"__BRIDGE_LOADED__"
 
 typedef void (^WVJBResponseCallback)(id responseData);
 typedef void (^WVJBHandler)(id data, WVJBResponseCallback responseCallback);
